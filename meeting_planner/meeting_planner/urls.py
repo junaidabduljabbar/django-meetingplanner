c3deea1921e2bf7c30edb33673034132795523da
Admin <junaid.abdul.jabbar@gmail.com>
2022-01-16 15:38:37 +0000

    added url for rooms_list

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from website.views import welcome,date
-from meetings.views import detail
+from meetings.views import detail, rooms_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',welcome),
+    path('',welcome, name="welcome"),
     path('date',date),
-    path('meetings/<int:id>',detail, name = "detail")
+    path('meetings/<int:id>',detail, name = "detail"),
+    path('rooms', rooms_list, name = "rooms_list")
 
 ]
