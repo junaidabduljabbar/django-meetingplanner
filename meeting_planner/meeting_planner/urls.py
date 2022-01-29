a540a6b592cbc4c34447cbe291c7d7b17e9e9d68
Junaid Abdul Jabbar <junaid.abdul.jabbar@gmail.com>
2022-01-29 12:09:54 +0000

    removed the meetings app urls

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from website.views import welcome,date
 from meetings.views import detail, rooms_list
 
@@ -22,7 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',welcome, name="welcome"),
     path('date',date),
-    path('meetings/<int:id>',detail, name = "detail"),
-    path('rooms', rooms_list, name = "rooms_list")
-
+    path('meetings/', include('meetings.urls')),
 ]
